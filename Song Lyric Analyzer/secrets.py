9d47a3ffed28c38b93061b1ff3c6b84bae7ff37d
Cliff Chen <c424chen@uwaterloo.ca>
2020-12-02 14:09:46 -0800

    Add files via upload

---
+++ Song Lyric Analyzer/secrets.py
@@ -0,0 +1,11 @@
+song = ["Enter Genius Links Here"
+        ]
+
+song_times = ["Enter Corresponding Song Times Here"
+              ]  # minutes and sec
+
+artist = ["Enter Corresponding Artists Here"
+          ]
+
+streams = ["Enter Corresponding Song Streams Here"
+           ]
